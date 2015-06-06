8a7accb8ee6a61c56fa3b49422045f7b18ac7640
richardburvill@hotmail.com <yco7uqkjf>
2015-06-06 14:21:30 +0800

    Dropping old facebook and fixing on mobile

--- packages/custom/lib/template_modules.js
+++ packages/custom/lib/template_modules.js
@@ -4,16 +4,6 @@ Telescope.modules.add("hero", {
   order: 1
 });
 
-//Telescope.modules.add("commentThreadBottom", {
-//    template: 'post_subscribe_facebook',
-//    order: 10
-//});
-
-//Telescope.modules.add("commentThreadBottom", {
-//    template: 'post_subscribe_facebookscript',
-//    order: 9
-//});
-
 Telescope.modules.add("commentThreadBottom", {
     template: 'post_share',
     order: 11
