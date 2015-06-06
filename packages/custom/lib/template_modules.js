67311404e71bcd0a3c27d2534966b59620aa65ae
richardburvill@hotmail.com <yco7uqkjf>
2015-06-06 11:11:26 +0800

    Kinda getting facebook going

--- packages/custom/lib/template_modules.js
+++ packages/custom/lib/template_modules.js
@@ -4,12 +4,12 @@ Telescope.modules.add("hero", {
   order: 1
 });
 
-//Telescope.modules.add("commentThreadBottom", {
-//    template: 'post_subscribe_facebook',
-//    order: 10
-//});
+Telescope.modules.add("commentThreadBottom", {
+    template: 'post_subscribe_facebook',
+    order: 10
+});
 
-//Telescope.modules.add("post_page", {
-//    template: 'post_subscribe_facebookscript',
-//    order: 11
-//});
+Telescope.modules.add("commentThreadBottom", {
+    template: 'post_subscribe_facebookscript',
+    order: 11
+});
