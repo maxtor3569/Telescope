ebfbfa2f4dd95b50f04da228310cbfc10aeac77b
Maxime <maxtor3569@gmail.com>
2015-08-05 09:46:02 +0200

    Fix bug undefined facebook id on start

--- packages/telescope-users/lib/server/create_user.js
+++ packages/telescope-users/lib/server/create_user.js
@@ -3,7 +3,8 @@ Accounts.onCreateUser(function(options, user){
   // get facebook profile picture
   console.log(options);
   if (options.profile && options.anonyme != true  ) {
-        options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
+        if(user.services.facebook)
+          options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
         user.profile = options.profile;
     }
   return user;
