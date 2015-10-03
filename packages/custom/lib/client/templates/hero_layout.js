2daf8226c5a1547914fcfe1545e7e9c96bc19e84
Judah Lamm <Judah.Lamm@gmail.com>
2015-10-03 00:31:01 -0400

    adds logic to submit message button on home page

--- packages/custom/lib/client/templates/hero_layout.js
+++ packages/custom/lib/client/templates/hero_layout.js
@@ -3,9 +3,19 @@ Meteor.startup(function() {
   Template.hero_layout.replaces('layout');
 
   Template.layout.events({
+    // 'click ' : function(e){
+
+    // },
       'click #submit_message_modal': function(e)
       {
-        Modal.show('submit_message_modal');
+        if (!Meteor.user()) {
+          Messages.flash(i18n.t('you_must_be_logged_in'), 'error');
+          console.log('not logged in');
+          Router.go('atSignIn');
+        } else {
+          Modal.show('submit_message_modal');
+        }
+
       }
   });
 });
